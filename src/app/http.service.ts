d3d2c2acac8a37016d5c8535d54442bae23a2249
ASHISH KOSHY <ashish.avior@gmail.com>
2023-02-11 07:29:58 +0530

    added environment config + http service

---
+++ src/app/http.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class HttpService {
+    constructor(private http: HttpClient) {}
+
+    private getApiBaseUrl(): string {
+        return environment.apiBaseUrl || '';
+    }
+
+    public get(path: string): Observable<unknown> {
+        return this.http.get(`${this.getApiBaseUrl()}/${path}`);
+    }
+
+    public post(path: string, body: unknown): Observable<unknown> {
+        return this.http.post(`${this.getApiBaseUrl()}/${path}`, body);
+    }
+
+    public put(path: string, body: unknown): Observable<unknown> {
+        return this.http.put(`${this.getApiBaseUrl()}/${path}`, body);
+    }
+
+    public patch(path: string, body: unknown): Observable<unknown> {
+        return this.http.patch(`${this.getApiBaseUrl()}/${path}`, body);
+    }
+
+    public delete(path: string): Observable<unknown> {
+        return this.http.delete(`${this.getApiBaseUrl()}/${path}`);
+    }
+}
