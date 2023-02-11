f6da3524cc56c97fd3042f8fa43e19f15cd81d5a
ASHISH KOSHY <ashish.avior@gmail.com>
2023-02-11 07:13:01 +0530

    added search and history routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        children: [
+            {
+                path: '',
+                pathMatch: 'full',
+                redirectTo: 'search',
+            },
+            {
+                path: 'search',
+                loadChildren: () =>
+                    import('./search/search.module').then(
+                        (m) => m.SearchModule
+                    ),
+            },
+            {
+                path: 'history',
+                loadChildren: () =>
+                    import('./history/history.module').then(
+                        (m) => m.HistoryModule
+                    ),
+            },
+        ],
+    },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
---
+++ src/app/search/search.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './search.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: SearchComponent,
+    },
+];
+
+@NgModule({
+    declarations: [SearchComponent],
+    imports: [CommonModule, RouterModule.forChild(routes)],
+})
+export class SearchModule {}
